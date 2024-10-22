56833172bf5ddbcd0902fb68178264675485289c
Kelvin Kipchumba <chogekelvin90@gmail.com>
2024-10-22 15:39:11 +0300

    employed operators to perform arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+//function multiply(num1, num2){
+ //   return num1 * num2;    
+    
+//}
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * (Number > 10) )+ 1;
+
+let num3 = 32;
+let num4 = 7;
+const mod = num3%num4
+
+const max = Math.max(1, 14, 11, 17, 20, 9 ,16,0.5 );
